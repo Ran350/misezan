bfb154dca5a99ada9bb37847de30fa23dcbe2ff9
Hoshi Ran <ureshinostyle350@gmail.com>
2023-12-28 03:15:50 +0900

    add: ts-parsec による四則演算器を実装した

--- src/__tests__/calculator.test.ts
+++ src/__tests__/calculator.test.ts
@@ -1,37 +1,15 @@
-import { calculate } from "../calculator";
+import assert = require("assert");
+import { evaluate } from "../calculator";
 
-test("test calculate()", () => {
-  // 四則演算ができる
-  expect(calculate("1+1")).toBe(2);
-  expect(calculate("1-1")).toBe(0);
-  expect(calculate("1*2")).toBe(2);
-  expect(calculate("4/2")).toBe(2);
-
-  // * / が優先される
-  expect(calculate("1*2+3")).toBe(5);
-  expect(calculate("1+2*3")).toBe(7);
-  expect(calculate("4/2+3")).toBe(5);
-  expect(calculate("3+4/2")).toBe(5);
-
-  // () が * / より優先される
-  expect(calculate("2*(2+3)")).toBe(10);
-  expect(calculate("2(2+3)")).toBe(10);
-  expect(calculate("(1+2)*3")).toBe(9);
-  expect(calculate("((2+1))")).toBe(3);
-
-  // 2桁以上の項にも対応する
-  expect(calculate("2+100(3+4*5)")).toBe(2302);
-
-  // 小数の演算にも対応する
-  expect(calculate("450*1.08")).toBe(486);
-
-  // 答えが小数になる演算にも小数第9位までは対応する
-  expect(calculate("1/3")).toBe(0.333333333);
-  expect(calculate("1.3*4.1")).toBe(5.33);
-
-  // 演算子ではなく正負の数としての +,- に対応する
-  expect(calculate("+1+2")).toBe(3);
-  expect(calculate("-1+2")).toBe(1);
-  expect(calculate("-1+(-2)")).toBe(-3);
-  expect(calculate("+1*(-2)")).toBe(-2);
+test(`Parser: calculator`, () => {
+  assert.strictEqual(evaluate("1"), 1);
+  assert.strictEqual(evaluate("+1.5"), 1.5);
+  assert.strictEqual(evaluate("-0.5"), -0.5);
+  assert.strictEqual(evaluate("1 + 2"), 3);
+  assert.strictEqual(evaluate("1-2"), -1);
+  assert.strictEqual(evaluate("1 * 2"), 2);
+  assert.strictEqual(evaluate("1 / 2"), 0.5);
+  assert.strictEqual(evaluate("1 + 2 * 3 + 4"), 11);
+  assert.strictEqual(evaluate("(1 + 2) * (((3 + 4)))"), 21);
+  assert.strictEqual(evaluate("1.2--3.4"), 4.6);
 });
